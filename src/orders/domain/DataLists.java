455b1addabfabec7799420262b39e44a792e3843
Czooi <126080176+Czooi@users.noreply.github.com>
2023-04-11 14:01:48 +0800

    Update DataLists.java

--- src/orders/domain/DataLists.java
+++ src/orders/domain/DataLists.java
@@ -3,7 +3,7 @@ package orders.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DataLists {
+public class DataLists implements IDataStore{
 
 	private List<Item> items;
 	
