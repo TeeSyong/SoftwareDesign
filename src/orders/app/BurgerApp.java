f278a34fe0255a9ec13c18fbd143da5cac41e66f
Czooi <126080176+Czooi@users.noreply.github.com>
2023-04-11 14:01:29 +0800

    Update BurgerApp.java

--- src/orders/app/BurgerApp.java
+++ src/orders/app/BurgerApp.java
@@ -1,11 +1,12 @@
 package orders.app;
 
-import orders.domain.Controller;
+import orders.domain.*;
 
 public class BurgerApp {
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
+		IDataStore dataLists = new DataLists();
 		Controller controller = new Controller();
+		controller.setDataLists(dataLists);
 		ConsoleUI userInterface = new ConsoleUI();
 		userInterface.setController(controller);
 		userInterface.start();
