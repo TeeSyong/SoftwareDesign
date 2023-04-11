551f6e816498c66eaf38090ea70019f96796a5fa
Czooi <126080176+Czooi@users.noreply.github.com>
2023-04-11 14:01:43 +0800

    Update Controller.java

--- src/orders/domain/Controller.java
+++ src/orders/domain/Controller.java
@@ -1,11 +1,22 @@
 package orders.domain;
 
+
+
+
+
 public class Controller {
 	private ViewInvoice viewInvoice = new ViewInvoice();
+	private IDataStore dataLists;
 
 	public Controller() {
-		//Initialize stuff
+		this.dataLists = null;
 	}
+	
+	public void setDataLists(IDataStore dataLists)
+	{
+		this.dataLists = dataLists;
+	}
+	
 	//THIS CLASS IS FOR CONTROLLER PURPOSE NOT WHOLE FUNCTION
 	
 	//CREATE USER PROFILE 
@@ -26,7 +37,15 @@ public class Controller {
 	
 	
 	//VIEW INVOICE
-	public void viewinvoice()
+	public void viewinvoice() {
 		viewInvoice.operation();
+	}
+		
 	//CHECK ORDER
+	public void checkOrder() {
+		
+	}
+	
+	
+	
 }
