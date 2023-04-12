7a79bb602bf44b88acbc87d4e6a6f80bdebef0ec
Czooi <126080176+Czooi@users.noreply.github.com>
2023-04-12 09:20:40 +0800

    Update Order.java

--- src/orders/domain/Order.java
+++ src/orders/domain/Order.java
@@ -4,13 +4,42 @@ import java.util.List;
 
 public class Order {
 
-	private int orderNo;
+	private int quantity;
+	private String foodName,orderId,itemCode,remark;
 	private Customer customer;
+	private Item item;
+	
 	
 	public Order(Customer customer) {
 		this.customer= customer;
 	}
-	public int getOrderNo() {
-		return orderNo;
+	
+	
+	public Order(String orderId, String itemCode, String foodName, int quantity, String remark ) {
+		this.orderId = orderId;
+		this.itemCode = itemCode;
+		this.foodName = foodName;
+		this.quantity = quantity;
+		this.remark = remark;
+	}
+	
+	public String getOrderId() {
+		return orderId;
+	}
+	
+	public String getItemCode() {
+		return itemCode;
+	}
+	
+	public String getFoodName() {
+		return foodName;
+	}
+	
+	public int getQuantity() {
+		return quantity;
+	}
+	
+	public String getRemark() {
+		return remark;
 	}
 }
